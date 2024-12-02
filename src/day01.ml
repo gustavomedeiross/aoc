8390fe379eec3b41d8158fe7144c9c8b3b2d3486
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2024-12-02 20:16:46 -0300

    Refactor day 01

--- src/day01.ml
+++ src/day01.ml
@@ -14,25 +14,41 @@ let unzip l =
   List.fold_left f ([], []) (List.rev l)
 ;;
 
+module IntMap = Map.Make (Int)
+
+let incr = ( + ) 1
+let map_fst f (x, y) = f x, y
+let map_snd f (x, y) = x, f y
+
 (** Part 1 *)
 
 let solve input =
-  let location_ids =
+  let locations =
     input
     |> String.split_on_char '\n'
     |> List.map String.trim
     |> remove_empty_lines
-    |> List.map (fun line ->
-      match line |> String.split_on_char ' ' |> List.filter (not % is_string_empty) with
-      | [ a; b ] -> int_of_string a, int_of_string b
-      | _ -> raise (Invalid_argument "Malformed line"))
+    |> List.map (fun line -> Scanf.sscanf line "%d %d" (fun x y -> x, y))
   in
-  let xs, ys = location_ids |> unzip in
+  let xs, ys = locations |> unzip in
+  (* Part 1 *)
   let xs, ys = List.sort Int.compare xs, List.sort Int.compare ys in
   List.combine xs ys
   |> List.map (fun (x, y) -> Int.abs (x - y))
   |> List.fold_left ( + ) 0
-  |> Format.printf "%i\n"
+  |> Format.printf "Part 1: %i\n";
+  (* Part 2 *)
+  (* fst is how many times it appears on the left, snd is on the right *)
+  let map = xs |> List.map (fun x -> x, (0, 0)) |> IntMap.of_list in
+  let count map (x, y) =
+    map
+    |> IntMap.update x (Option.map (map_fst incr))
+    |> IntMap.update y (Option.map (map_snd incr))
+  in
+  let map = List.fold_left count map locations in
+  0
+  |> IntMap.fold (fun loc (xs, ys) total -> (loc * xs * ys) + total) map
+  |> Format.printf "Part 2: %i\n"
 ;;
 
 let%expect_test _ =
@@ -43,58 +59,12 @@ let%expect_test _ =
      1   3
      3   9
      3   3
-     |};
-  [%expect "11"]
-;;
-
-(** Part 2 *)
-
-module IntMap = Map.Make (Int)
-
-let solve2 input =
-  let location_ids =
-    input
-    |> String.split_on_char '\n'
-    |> List.map String.trim
-    |> remove_empty_lines
-    |> List.map (fun line ->
-      match line |> String.split_on_char ' ' |> List.filter (not % is_string_empty) with
-      | [ a; b ] -> int_of_string a, int_of_string b
-      | _ -> raise (Invalid_argument "Malformed line"))
-  in
-  let xs, _ys = location_ids |> unzip in
-  (* fst is how many times it appears on the left, snd is on the right list *)
-  let map = xs |> List.map (fun x -> x, (0, 0)) |> IntMap.of_list in
-  let map =
-    List.fold_left
-      (fun map (x, y) ->
-        map
-        |> IntMap.update y (function
-          | None -> None
-          | Some (x, y) -> Some (x, y + 1))
-        |> IntMap.update x (function
-          | None -> None
-          | Some (x, y) -> Some (x + 1, y)))
-      map
-      location_ids
-  in
-  let result =
-    IntMap.fold (fun location (xs, ys) total -> (location * xs * ys) + total) map 0
-  in
-  Format.printf "%i\n" result
-;;
-
-let%expect_test _ =
-  solve2 {|
-     3   4
-     4   3
-     2   5
-     1   3
-     3   9
-     3   3
-     |};
-  [%expect "31"]
+  |};
+  [%expect {|
+     Part 1: 11
+     Part 2: 31
+  |}]
 ;;
 
 let read_file file = In_channel.with_open_bin file In_channel.input_all
-let main () = read_file "./input/day01.txt" |> solve2
+let main () = read_file "./input/day01.txt" |> solve
